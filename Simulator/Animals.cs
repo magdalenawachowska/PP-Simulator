99b8b66952403bdcbe20c5b4e2f8953ad9e9e172
magdalenawachowska <232483@student.uek.krakow.pl>
2024-10-29 22:07:12 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -10,9 +10,32 @@ namespace Simulator;
 
 public class Animals             
 {
-    public required string Description { get; init; }
-    public uint Size { get; set; } = 3;
 
+    private string description = "Unknown";
+    public required string Description
+    { 
+        get => description; 
+        init
+        {
+            description = value.Trim();                  // usuwa zbedne spacje
+
+            if (description.Length < 3)
+            {
+                description = description.PadRight(3, '#');
+            }
+            else if (description.Length > 15)
+            {
+                description = description.Substring(0, 15);
+                description = description.Trim();
+                if (description.Length < 3)
+                {
+                    description = description.PadRight(3, '#');
+                }
+            }
+            description = char.ToUpper(description[0]) + description.Substring(1);
+        }
+    }
+    public uint Size { get; set; } = 3;
 
     public string Info               // wlasciwosc do odczytu 
     {
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -7,35 +7,69 @@ using System.Threading.Tasks;
 
 namespace Simulator;
 
-internal class Creature
+public class Creature
 {
-    private string name;           // field
-    private int level;            // field
+    private string name = "Unknown";           // field
+    private int level = 1;                    // field
 
-    public string Name            // property - uzywane potem w konstruktorze !
+    public string Name
     {
-        get; set;                // short-hand
-    }                           
-    public int Level
-    {
-        get { return level; }
-        set { level = value; }
-    }
+        get => name;
+        init                                      // init - uzywane zeby wartosc mogla byc nadana tylko raz przy inicjacji
+        {
+            name = value.Trim();                  // usuwa zbedne spacje
+            
+            if (name.Length <3)
+            {
+                name = name.PadRight(3, '#');
+            }
+            else if (name.Length >25)
+            {
+                name = name.Substring(0,25);
+                name = name.Trim();
+                if (name.Length < 3)
+                {
+                    name = name.PadRight(3, '#');
+                }
+            }
+            name = char.ToUpper(name[0]) + name.Substring(1);      // zamiana pierwszej litery na wielka
+        }
+    }                                             // property - uzywane potem w konstruktorze !
 
-    public Creature()             // bezparametrowy konstruktor "nic nie robiacy"
+    public int Level                             // **int na uint- zeby nie bylo ujemnych
+    {
+        get => level;
+        init
+        {
+            if (value < 1)
+                value = 1;
+            else if (value > 10)
+                value = 10;
+            level = value;
+        }
+                                                 
+    }   
+
+    public Creature()                
     {   }
 
-    public Creature(string name, int level= 1)         
+    public Creature(string name, int level=1)         
     {
         Name = name;
         Level = level; 
     }
 
-    public string Info               // wlasciwosc do odczytu 
+    public string Info                               // wlasciwosc do odczytu 
     {
         get { return $"{Name} [{Level}]"; }
     }
 
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}");
 
+    public void Upgrade()                
+    {
+        // pozwoli na awansowanie stwora o jeden poziom, ale nie powyzej 10go
+
+        level = Level >= 10 ? Level : Level + 1;    
+    }
 }
